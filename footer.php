6afdf39beff94a6ed3213492fb833064588ae555
BagusSjm <bagusibag85@gmail.com>
2016-08-28 11:23:36 +0700

    file template website digtive procedural

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!-- bagian footer -->
+         <div class="d-nav clear" id="d-fNav">
+             <a href="https://www.bagussjm.github.io"><div id="d-brand" style="margin-top : 10px">
+                 
+                  <img src="asset/brand.png"  style="float: left, margin: 0 5% 0 10px" >
+                  <span>Digtive</span>
+                 
+             </div></a>
+             
+             
+             <a href="#" class="nav-lst fLst">Blog</a>
+             <a href="#" class="nav-lst fLst">Komunitas</a>
+             <a href="#" class="nav-lst fLst">Kontak</a>
+             <a href="#" class="nav-lst fLst">Tentang</a>
+             <a target="_blank" href="https://bagussjm.github.io" class="fB2" style="padding-right : 10px"> 
+                 Design by Bagus sjm
+             </a>
+             <span class="fB2">&COPY; 2016 Digtive Developer</span>   
+             
+         </div>
+     </body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+     <head>
+          <meta charset="utf-8">
+          <title>design digtive alpha version</title>
+          <link rel="stylesheet" href="style.css" media="screen"  charset="utf-8">
+          <link rel="stylesheet" href="unveril.css" media="screen"  charset="utf-8">
+          <link href='https://fonts.googleapis.com/css?family=Oxygen+Mono' rel='stylesheet' type='text/css'>
+          <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="unveril.js"></script> 
+     </head>
+     <body class="role-model">
+         
+         <!-- bagian header -->
+         <div class="d-nav bxSh" id="d-hNav">
+             <a href="https://www.bagussjm.github.io"><div id="d-brand" style="margin-top : 10px">
+                 
+                  <img src="asset/brand.png"  style="float: left, margin: 0 5% 0 10px" >
+                  <span>Digtive</span>
+                 
+             </div></a>
+             
+             <a href="#" class="nav-lst hLst">Home</a>
+             <a href="#" class="nav-lst hLst">Blog</a>
+             <a href="#" class="nav-lst hLst">Lab</a>
+             <a href="#" class="nav-lst hLst">Stack</a>
+             <a href="#" class="nav-lst hLst">Daily</a>
+                
+         </div>
