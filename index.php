1574f073e842fac00988e2db53c56716a0dd9b6a
BagusSjm <bagusibag85@gmail.com>
2016-08-28 09:28:04 +0700

    file landing page import dari directory design

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+     <head>
+          <meta charset="utf-8">
+          <title>design digtive alpha version</title>
+          <link rel="stylesheet" href="style.css" media="screen"  charset="utf-8">
+          <link rel="stylesheet" href="unveril.css" media="screen"  charset="utf-8">
+          <link href='https://fonts.googleapis.com/css?family=Oxygen+Mono' rel='stylesheet' type='text/css'>
+          <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+          <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="unveril.js"></script>
+     </head>
+     <body>
+         <div id="Frst" class="b">
+             <div class="dIntr">
+                  <div class="container" style="padding:30px 0 30px 0">
+                       <div class="navLndng">
+                         <a href="https://www.bagussjm.github.io"><div id="Lbrnd" style="margin-top : 10px" class="Lbrnd">
+                         <img src="asset/brand.png"  style="float: left, margin: 0 5% 0 10px" >
+                         <span>Digtive</span>
+                          </div></a>
+
+
+                           <a href="#" class="lLst">Daily</a>
+                           <a href="#" class="lLst ">Stack</a>
+                           <a href="#" class="lLst">Lab</a>
+                           <a href="#" class="lLst">Blog</a>
+
+                       </div>
+
+                       <div class="d-col2 clear" style="padding-left:20px; line-spacing:normal;margin-top:60px;height:400px">
+                          <h1 class="txtIntr" style="font-size:50px;margin-bottom:0">Digtive <br> <h3 class="txtIntr" style="font-size:30px">Think & Launch</h3></h1><br>
+                          <h5 class="txtIntr" style="font-size:25px; padding-top:25px">Selesaikan berbagai masalah bersama,<br>berfikir kreatif dan wujudkan dalam <br> dunia <b style="font-weight:bold;font-size:28px">Digital</b></h5>
+
+                         <a href="https://www.facebook.com/DigtiveDevOps/"  target="_blank" class="Scmd"> <i class="fa fa-facebook "></i></a>
+                         <a href="#" class="Scmd"> <i class="fa fa-twitter "></i></a>
+                         <a href="#" class="Scmd"> <i class="fa fa-google-plus"></i></a>
+                       </div>
+
+                       <div class="d-col2" style="text-align:center;padding-left:20%;margin-top:80px;height:400px">
+                         <div class="anch"><a href="#Thrd" class="scroll">Fitur</a></div>
+                         <div class="anch"><a href="#Frth" class="scroll">Team</a></div>
+                         <div class="anch"><a href="#Fvth" class="scroll">Info</a></div>
+                       </div>
+                  </div>
+             </div>
+         </div>
+
+         <div id="Scnd" >
+              <div class="container" style="padding-top:25px">
+                   <h1 class="a2" style="color:rgb(69, 204, 233);">Lebih dekat dengan Digtive</h1>
+
+                   <div class="d-col2" style="height:200px;margin-top:100px;">
+                        <img src="asset/komunitas.png" alt="intro quotes by digtive" style="float:left;margin-right:17px"/>
+                        <p style="font-size:21px;color:rgb(69, 204, 233);padding-top:35px">
+                             Digtive merupakan sarana berbagi masalah sehari-hari yang dijumpai di sekitar dan berusaha memberi solusi bersama sebagai komunitas.
+                        </p>
+                   </div>
+
+                   <div class="d-col2" style="height:200px;margin-top:100px;">
+                        <img src="asset/team.png" alt="intro quotes by digtive" style="float:left;margin-right:17px" />
+                        <p style="font-size:21px;color:rgb(69, 204, 233);padding-top:35px">
+                             Digtive membantu melatih kerja sama tim dalam menyelesaikan tugas melalui berbagai fitur Digtive.
+                        </p>
+                   </div>
+
+                   <div class="d-col2" style="height:200px;margin-top:100px;">
+                        <img src="asset/eksperimen.png" alt="intro quotes by digtive" style="float:left;margin-right:17px" /><p style="font-size:21px;color:rgb(69, 204, 233);padding-top:35px">
+                             Team Digtive selalu ber eksperimen untuk membuat program-program yang terinspirasi dari studi kasus yang telah di pelajari.
+                        </p>
+                   </div>
+
+                   <div class="d-col2" style="height:200px;margin-top:100px;">
+                        <img src="asset/edukasi.png" alt="intro quotes by digtive" style="float:left;margin-right:17px"/><p style="font-size:21px;color:rgb(69, 204, 233);padding-top:35px">
+                             Digtive berusaha menjadi salah satu media edukasi dalam dunia digital.
+                        </p>
+                   </div>
+
+              </div>
+         </div>
+
+         <div id="Thrd" >
+              <div class="container" style="padding-top:25px">
+                   <h1 class="a2" style="color:white">Jelajahi berbagai fitur Digtive</h1>
+
+                   <div class="d-col4 lFtr bxR" style="height:200px">
+                        <img src="asset/blog.png" alt="fitur by Digtive" style="width:100%;height:100%"/>
+                        <h1 class="a3" style="font-size:25px;">Digtive Blog <p>Tulis dan arsipkan segala ide kreatif di Digitve Blog </p><a href="#" >lihat</a></h1>
+
+
+                   </div>
+
+                   <div class="d-col4 lFtr bxR" style="height:200px">
+                        <img src="asset/lab.png" alt="fitur by Digtive" style="width:100%;height:100%"/>
+                        <h1 class="a3" style="font-size:25px;">Digtive Lab <p>Lihat hal-hal baru yang di kembangkan oleh Creative Crew  </p><a href="#" >lihat</a></h1>
+                   </div>
+
+                   <div class="d-col4 lFtr bxR" style="height:200px">
+                        <img src="asset/stack.png" alt="fitur by Digtive" style="width:100%;height:100%"/>
+                         <h1 class="a3" style="font-size:25px;">Digtive Stack <p>cari berbagai permasalahan dan solusi  yang dapat membantu anda</p><a href="#">lihat</a></h1>
+                   </div>
+
+                   <div class="d-col4 lFtr bxR" style="height:200px">
+                        <img src="asset/daily.png" alt="fitur by Digtive" style="width:100%;height:100%"/>
+                         <h1 class="a3" style="font-size:25px;">Digtive Daily <p>Lihat berbagai even yang di adakan dan yang sedang terjadi </p><a href="#">lihat</a></h1>
+                   </div>
+
+              </div>
+         </div>
+
+         <div id="Frth" >
+              <div class="container" style="padding-top:25px">
+                   <h1 class="a2" style="color:white">Creative Crew</h1>
+
+                   <div class="d-col2" style="margin-top:30px;height:375px;padding-top:32px">
+                        <img src="asset/eksperimen.png" alt="" style="float:left;height:80%;margin-right:5%"/>
+                        <p class="a4">
+                             Creative Crew terus berusaha memperbaiki fitur-fitur Digtive dengan tool modern yang terus di kembangkan <br><br><b style="font-size:20px">-CREATIVE CREW</b>
+                        </p>
+                   </div>
+
+                   <div class="d-col2" style="margin-top:30px;height:375px">
+                        <h1 class="a2" style="color:white">Kontributor Digtive</h1>
+                        <div class="d-pnl "style="height:300px;overflow-y:scroll;margin-top:20px;background-color:inhinherit">
+                             <div class="d-pnl bxR wB" style="height:100px">
+                                  <img src="asset/ibag.png" alt="kontributor Digtive" class="a41"/>
+                                  <a  href="https://twitter.com/Sujatmiko22" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Bagus Sujatmiko <br><span style="color:rgba(0, 0, 0, 0.44)">CEO & founder Digtive <b style="font-size:23px">|</b> UIN SUSKA RIAU</span></a>
+                             </div>
+                             <div class="d-pnl bxR wB" style="height:100px;margin-top:20px">
+                                  <img src="asset/team_novri.jpg" alt="kontributor Digtive" class="a41 "/>
+                                  <a  href="https://www.facebook.com/novri.kurniawan.94" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Novri Kurniawan <br><span style="color:rgba(0, 0, 0, 0.44)">Co Design <b style="font-size:23px">|</b>  UIN SUSKA RIAU</span></a>
+                             </div>
+                             <div class="d-pnl bxR wB" style="height:100px;margin-top:20px">
+                                  <img src="asset/team_haris.jpg" alt="kontributor Digtive" class="a41 "/>
+                                  <a  href="https://www.facebook.com/abdul.haris.946179" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Abdul Haris<br><span style="color:rgba(0, 0, 0, 0.44)">Senior Program <b style="font-size:23px">|</b>  UIN SUSKA RIAU</span></a>
+                             </div>
+                             <div class="d-pnl bxR wB" style="height:100px;margin-top:20px">
+                                  <img src="asset/team_andre.jpg" alt="kontributor Digtive" class="a41 "/>
+                                  <a  href="https://twitter.com/AndreVari27" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Andre Vari Antoni<br><span style="color:rgba(0, 0, 0, 0.44)">Tutor Manager <b style="font-size:23px">|</b>  UIN SUSKA RIAU</span></a>
+                             </div>
+                             <div class="d-pnl bxR wB" style="height:100px;margin-top:20px">
+                                  <img src="asset/team_sidiq.jpg" alt="kontributor Digtive" class="a41 "/>
+                                  <a  href="https://twitter.com/aazar36" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Azhar Sidiq<br><span style="color:rgba(0, 0, 0, 0.44)">CO product <b style="font-size:23px">|</b>  UIN SUSKA RIAU</span></a>
+                             </div>
+                             <div class="d-pnl bxR wB" style="height:100px;margin-top:20px">
+                                  <img src="asset/team_ryan.jpg" alt="kontributor Digtive" class="a41 "/>
+                                  <a  href="https://www.facebook.com/ryan.dwijaya2" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Ryan Dwijaya<br><span style="color:rgba(0, 0, 0, 0.44)">Co Marketing <b style="font-size:23px">|</b>  UIN SUSKA RIAU</span></a>
+                             </div>
+                             <div class="d-pnl bxR wB" style="height:100px;margin-top:20px">
+                                  <img src="asset/team_jihad.jpg" alt="kontributor Digtive" class="a41 "/>
+                                  <a  href="https://www.facebook.com/jiad.benastey" style="font-size:23px;color:rgb(47, 47, 46);font-family:'Oxygen Mono';font-weight:lighter" target="_blank">Jihad Benastey<br><span style="color:rgba(0, 0, 0, 0.44)">Head of UX/UI <b style="font-size:23px">|</b>  UIN SUSKA RIAU</span></a>
+                             </div>
+                        </div>
+                   </div>
+
+              </div>
+         </div>
+
+         <div id="Fvth" >
+              <div class="container" style="padding-top:30px">
+                   <h1 class="a2" style="color:#371e1e">Terus berkarya bersama Digtive</h1>
+
+                   <div class="d-col2 d-pnl" style="height:420px;margin-top:28px">
+                        <h2  style="color:#371e1e; font-size:22px;margin-bottom:20px"> Bergabung bersama Creative Crew</h2>
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d894.0274186702445!2d101.41912818677842!3d0.44682520887783517!2m3!1f270!2f41.438963935929884!3f0!3m2!1i1024!2i768!4f20!5e1!3m2!1sid!2sid!4v1456718331076" frameborder="0" style="border:0;width:100%;height:350px" allowfullscreen>
+                       </iframe>
+                   </div>
+
+                   <div class="d-col2 d-pnl" style="height:420px;margin-top:28px">
+                        <h2  style="color:#371e1e; font-size:22px;margin-bottom:20px"> kirim masukan dan saran anda ke Creative Crew</h2>
+
+                        <form class=""  method="post">
+                             <input type="text" name="name" placeholder="nama..." class="d-inp d-col2 bxR">
+                             <input type="text" name="name" placeholder="email..." class="d-inp d-col2 bxR">
+                             <textarea name="name" rows="8" cols="40" class="d-inp bxR" style="height:150px;width:100%" placeholder="kirim saran anda .."></textarea>
+                             <input type="button" name="send" value="kirim" class="d-inp bxR wB  d-sbmt">
+                        </form>
+
+                        <div class="d-col3" style="clear:both">
+                             <h2  style="color:rgba(0, 0, 0, 0.79); font-size:22px;margin-bottom:13px">info</h2>
+                             <li class="a31">info.digtive@gmail.com</li>
+                             <li class="a31">+628 2174 6143 90</li>
+                             <li class="a31">Jl. Soekarno Hatta </li>
+                        </div>
+                        <div class="d-col3" >
+                             <h2  style="color:rgba(0, 0, 0, 0.79); font-size:22px;margin-bottom:13px">media</h2>
+                             <a href="https://www.facebook.com/DigtiveDevOps/" target="_blank"><li class="a31"><i class="fa fa-facebook a32"></i>Digtive DevOps</li></a>
+                             <a href="#"><li class="a31"><i class="fa fa-wechat a32"></i>Digtive Oficcial</li></a>
+                             <a href="#"><li class="a31"><i class="fa fa-twitter a32"></i>Team Digtive</li></a>
+                        </div>
+                        <div class="d-col3" >
+                             <h2  style="color:rgba(0, 0, 0, 0.79); font-size:22px;margin-bottom:13px">Team</h2>
+                              <a href="#"><li class="a31">Blog</li></a>
+                             <a href="#"><li class="a31">Tentang</li></a>
+                             <span class="a31" style="list-syle:none">&copy; 2016 Digtive Developer</span>
+                        </div>
+
+                   </div>
+
+
+              </div>
+        </div>
+
+     </body>
+</html>
