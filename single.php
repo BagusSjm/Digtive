1704f3b6a5e48e732176775a58f13765ee8fee66
BagusSjm <bagusibag85@gmail.com>
2016-09-10 22:06:09 +0700

    file untuk menampilkan post per id

---
+++ single.php
@@ -0,0 +1,24 @@
+<?php require_once 'header.php'; ?>
+
+<div class="container">
+     <!-- main panel -->
+        <div class=" d-pnl mTp1 bxSh wB">
+            
+            
+                <div class=" d-pnl ">
+                <!-- post on blog -->   
+                <a href="#"><p  class="d-lb lbMd bB bxR"  style="text-align: center">Aktivitas team Digtive di socmed</p></a>
+                        
+                      
+                <img src="asset/daily.png" style="width: 20%;height: 20%;margin: 15px auto">
+                          <p class=" txtPrv " >
+                               Creative Crew, merupakan sebuatan untuk pengembang (developer) dan juga merupakan data 
+                               besar yang di kembangkan dan terus di dokumentasikan di dalam sebuah web direktori ...
+                          </p>
+
+                </div>
+            
+            
+            <div class="clear"></div>
+        </div>
+</div>
